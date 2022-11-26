7fb7e9976369a8b1051f6c7545f51533691b4540
BAMAS Mathias <113921847+mathiasbamas@users.noreply.github.com>
2022-11-26 11:17:44 +0100

    Add files via upload

--- SystemesDeVotes/SystemeFrancais/main.cpp
+++ SystemesDeVotes/SystemeFrancais/main.cpp
@@ -4,8 +4,7 @@
 
 using namespace std;
 
-/* BAMAS - BIHI - CHOPARD - BERGERON - ALI - SAE R1.01
- * L'election a la française consiste a voté un premier tour pour elire un candidat
+/* L'election a la française consiste a voté un premier tour pour elire un candidat
  * si un candidat arrive à la majoritée absolue (> 50%) il est elu directement sinon un
  * second tour a lieu entre les 2 premiers candidats */
 
@@ -81,7 +80,10 @@ int main()
 {
     vector<vector<unsigned>> listeVotes = recupVoteTotal();
     vector<unsigned> resultatPremierTour = resultatTour(listeVotes,1);
-
+    cout << "Resultat :" << endl << endl;
+    for(unsigned i = 0; i < resultatPremierTour.size(); ++i){
+        cout << listeJeux[i] << " " << resultatPremierTour[i] << " voix" << endl;
+    }
     for(unsigned i = 0; i < resultatPremierTour.size(); ++i)
     {
         if(resultatPremierTour[i] > listeVotes.size()/2) // Vrai -> gagnant au premier tour, fin de l'election
@@ -94,7 +96,9 @@ int main()
     // Second tour
     vector<string> listeJeuxSecondTour = trouveGagnantPremierTour(resultatPremierTour);
     vector<unsigned> resultatSecondTour = resultatTour(listeVotes,2);
-    cout << "Second tour entre " << endl << "0 - " << listeJeuxSecondTour[0] << endl << "1 - " << listeJeuxSecondTour[1] << endl;
+    cout << endl << "Second tour entre " << endl << "0 - " << listeJeuxSecondTour[0] << endl << "1 - " << listeJeuxSecondTour[1] << endl << endl;
+    cout << listeJeuxSecondTour[0] << " " << resultatSecondTour[0] << " voix" << endl;
+    cout << listeJeuxSecondTour[1] << " " << resultatSecondTour[1] << " voix" << endl << endl;
     cout << "Vainqueur : ";
     unsigned m = max(resultatSecondTour[0],resultatSecondTour[1]);
     // Find permet de trouver l'indice du jeu gagnant donné par max
